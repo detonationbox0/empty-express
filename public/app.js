20639ba52e1680758e7da9c2343da2c17adcef71
detonationbox0 <detonationbox0@live.com>
2023-03-08 10:51:50 -0500

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,10 @@
+$(() => {
+    console.log("Loaded. Let's rock! 🤘");
+});
+
+/**
+ * User clicks "Talk to Servevr"
+ */
+$("#talk").on("click", () => {
+    alert("Talk to the server...")
+})
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"))
+
+
+// Server code here...
+
+
+
+app.listen(3000);
